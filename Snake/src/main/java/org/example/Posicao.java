1487130cff9bf9e203656a748bb7e440593864f7
Jose <a045146@umaia.pt>
2024-12-02 18:55:24 +0000

    Posicao

--- Snake/src/main/java/org/example/Posicao.java
+++ Snake/src/main/java/org/example/Posicao.java
@@ -1,5 +1,33 @@
 package org.example;
 
 public class Posicao {
+    private int x;
+    private int y;
 
+    public Posicao(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicao posicao = (Posicao) o;
+        return x == posicao.x && y == posicao.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
 }
+
