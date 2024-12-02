c50032b751b28af3a8578358b262a2b17991caf4
Diogo Teixeira <A044483@umaia.pt>
2024-12-02 18:44:13 +0000

    Gamelauncher

--- Snake/src/main/java/org/example/Gamelauncher.java
+++ Snake/src/main/java/org/example/Gamelauncher.java
@@ -1,5 +1,11 @@
 package org.example;
 public class Gamelauncher {
-
+    public static void main(String[] args) {
+        final int LARGE_WIDTH = 80;
+        final int LARGE_HEIGHT = 23;
+        System.out.println("Começa o jogo com a largura " + LARGE_WIDTH + " e altura: " + LARGE_HEIGHT);
+        GameView game = new GameView(LARGE_WIDTH, LARGE_HEIGHT);
+        game.openMainMenu();
+    }
 
 }
